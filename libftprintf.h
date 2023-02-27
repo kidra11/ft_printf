680aa7665bd9dd354c4492294250803b06dae478
Nathalie Sion <nsion@c2r4p10.42perpignan.fr>
2023-02-27 16:29:45 +0100

    finish

--- libftprintf.h
+++ libftprintf.h
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/24 18:06:27 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/27 16:21:21 by nsion            ###   ########.fr       */
+/*   Created: 2023/02/27 16:29:16 by nsion             #+#    #+#             */
+/*   Updated: 2023/02/27 16:29:20 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
@@ -19,20 +20,20 @@
 # include <string.h>
 # include <stdlib.h>
 
-int		ft_printf(const char *s, ...);
+int	ft_printf(const char *s, ...);
 
-int		ft_len(char *s);
+int	ft_len(char *s);
 
-int		ft_putchar(char c);
+int	ft_putchar(char c);
 
-int		ft_putstr(char *s);
+int	ft_putstr(char *s);
 
-int		for_p(unsigned long long int *n, char *base, int num);
+int	for_p(unsigned long long int *n, char *base, int num);
 
-int		ft_putnbr_base(int n, char *base, int num);
+int	ft_putnbr_base(int n, char *base, int num);
 
-int		ft_unput(unsigned int n, char *base, int num);
+int	ft_unput(unsigned int n, char *base, int num);
 
-int		find(char s, va_list trois, int num);
+int	find(char s, va_list trois, int num);
 
 #endif
